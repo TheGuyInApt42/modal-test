adc855b69df6429c7141f27428bb1115ed911889
TheGuyInApt42 <blackgandalf83@gmail.com>
2021-12-09 17:37:59 -0500

    First commit

---
+++ src/lib/shared/ui/components/modal/Content.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { createEventDispatcher } from 'svelte';
+
+  const dispatch = createEventDispatcher();
+  function showSurprise() {
+        console.log('here');
+        dispatch('showSurprise');
+  }
+</script>
+
+
+<button
+    class="inline-block px-4 py-2 ml-auto font-medium text-black uppercase 
+    shadow-md bg-turquoise-accent lg:ml-0 lg:w-max dark:bg-orange-accent"
+    on:click={() => showSurprise()} >
+    <span
+        class="text-sm opacity-75">Learn More
+    </span>
+    
+</button>
+
